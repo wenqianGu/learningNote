5d022dc03d73bc4ec8e5b3ee523c39e50af77ee3
wenqianGU <guwenqian10@gmail.com>
2023-03-02 16:47:01 +1100

    simple react app

---
+++ 11.UseContext/use_context/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Index } from "./pages";
+import { About } from "./pages/about";
+
+
+function AppRouter() {
+  return (
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+        </ul>
+      </nav>
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+    </div>
+  </Router>
+  );
+}
+
+export default AppRouter;
